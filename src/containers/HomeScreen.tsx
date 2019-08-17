d6dbfffe5cd19f2d2f8e1ea78befeac29964a4ac
lotosbin <lotosbin@gmail.com>
2019-08-17 16:23:04 +0800

    fix: warning

--- src/containers/HomeScreen.tsx
+++ src/containers/HomeScreen.tsx
@@ -5,6 +5,7 @@ import {useNavigation} from "react-navigation-hooks";
 import {Icon} from 'react-native-material-ui';
 
 interface TApp {
+    id: any,
     name: string,
     uri: string,
     icon_name?: string,
@@ -33,30 +34,36 @@ export const HomeScreen = () => {
     const [apps] = useState<TApp[]>(() => {
         return [
             {
+                id: 0,
                 name: 'Demo',
                 uri: 'http://www.baidu.com',
             },
             {
+                id: 1,
                 name: '笑话',
                 uri: 'http://joke.yuanjingtech.com',
                 icon_name: "smile-o"
             },
             {
+                id: 2,
                 name: '远景',
                 uri: 'http://www.yuanjingtech.com',
                 icon_name: 'star-o'
             },
             {
+                id: 3,
                 name: '导航',
                 uri: 'http://daohang.binbinsoft.com/',
                 icon_name: 'internet-explorer'
             },
             {
+                id: 4,
                 name: '优惠',
                 uri: 'http://youhui.yuanjingtech.com/',
                 icon_name: "tags"
             },
             {
+                id: 5,
                 name: "更多",
                 uri: "http://www.yuanjingtech.com/more.html",
                 icon_name: "bars"
@@ -65,7 +72,7 @@ export const HomeScreen = () => {
     });
     return (
         <View style={styles.container}>
-            {apps.map((v: TApp) => <AppItem key={`${v}`} data={{name: v.name, uri: v.uri, icon_name: v.icon_name}}/>)}
+            {apps.map((v: TApp) => <AppItem key={`${v.id}`} data={{name: v.name, uri: v.uri, icon_name: v.icon_name}}/>)}
             <View style={{flex: 1}}></View>
         </View>
     );
