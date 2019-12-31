120f4118accc8036c41493e7f91f68af20924341
liubinbin <lotosbin@gmail.com>
2019-12-31 12:28:42 +0800

    feat: add activities

--- src/containers/GetCoinScreen.tsx
+++ src/containers/GetCoinScreen.tsx
@@ -3,6 +3,7 @@ import {View} from "react-native";
 import CoinViewRewardAd from "../modules/coin/containers/CoinViewRewardAd";
 import {Button} from "react-native-material-ui";
 import {useNavigation} from "@react-navigation/native";
+import ActivityListContainer from "../../libs/activities/containers/ActivityListContainer";
 
 
 const GetCoinScreen = () => {
@@ -11,6 +12,7 @@ const GetCoinScreen = () => {
         <View>
             <CoinViewRewardAd/>
             <Button text={"领红包"} onPress={() => navigate("MyWebView", {uri: "https://s.click.taobao.com/boQJssv"})}/>
+            <ActivityListContainer/>
         </View>
     );
 };
