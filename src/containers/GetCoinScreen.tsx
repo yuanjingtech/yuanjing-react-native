f0ab7df165a90ca3ee87584d57d4d11dcbb3029c
liubinbin <lotosbin@gmail.com>
2019-12-31 16:16:25 +0800

    feat: add ad

--- src/containers/GetCoinScreen.tsx
+++ src/containers/GetCoinScreen.tsx
@@ -4,12 +4,14 @@ import CoinViewRewardAd from "../modules/coin/containers/CoinViewRewardAd";
 import {Button} from "react-native-material-ui";
 import {useNavigation} from "@react-navigation/native";
 import ActivityListContainer from "../../libs/activities/containers/ActivityListContainer";
+import MyAdBannerWithReward from "../components/MyAdBanner";
 
 
 const GetCoinScreen = () => {
     const {navigate} = useNavigation();
     return (
         <View>
+            <MyAdBannerWithReward/>
             <CoinViewRewardAd/>
             <Button text={"领红包"} onPress={() => navigate("MyWebView", {uri: "https://s.click.taobao.com/boQJssv"})}/>
             <ActivityListContainer/>
